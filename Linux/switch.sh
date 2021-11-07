4262e3585745635a186e48bae4068ea03010a2f5
SimonGino <68073491+SimonGino@users.noreply.github.com>
2021-11-07 14:30:34 +0800

    Update switch.sh

--- Linux/switch.sh
+++ Linux/switch.sh
@@ -4,7 +4,7 @@ source /etc/profile
 
 # Variation
 Interface=wg
-Region=sg
+Region=tw
 Hostname=
 Telegram_Token=928141567:AAGmjiM0TjM5f05DlnScgS6s5M3Buc3rPK4
 Telegram_ChatID=1093460198
@@ -17,23 +17,33 @@ function Start {
     echo -e " [Intro] Test System:Ubuntu 20"
     echo -e " [Intro] OpenSource-Project:https://github.com/acacia233/Project-WARP-Unlock"
     echo -e " [Intro] Telegram Channel:https://t.me/cutenicobest"
-    echo -e " [Intro] Version:2021-10-7-1"
+    echo -e " [Intro] Version:2021-11-3-1"
     Test_Netflix_Access
 }
 
 function Test_Netflix_Access {
-    local result1="$(curl --interface $Interface --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 5 "https://www.netflix.com/title/81215567" 2>&1)"
-    local result2="$(curl --interface $Interface --user-agent "${UA_Browser}" -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | cut -d '/' -f4 | cut -d '-' -f1 2>&1)"
-    if [[ "$result1" == "200" ]] && [[ "$result2" == "$Region" ]]; then
-        Judge
-    else
+    result1="$(curl --interface $Interface --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 5 "https://www.netflix.com/title/81215567" 2>&1)"
+    if [[ "$result1" == "200" ]]; then
+        result2="$(curl --interface $Interface --user-agent "${UA_Browser}" -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | cut -d '/' -f4 | cut -d '-' -f1 2>&1)"
+        if [[ "$result2" == "$Region" ]]; then
+            ErrorCount=0
+            Judge
+        fi
+    elif [[ "$result1" == "403" ]]; then
         ChangeIP
+    else
+        let ErrorCount++
+        if [[ $ErrorCount == 5 ]];then
+            curl -s -X POST $URL_Message -d chat_id=$Telegram_ChatID -d text="Node:$Hostname%0ANeed to manually check" >/dev/null
+            break
+        fi
+        Judge
     fi
 }
 
 function ChangeIP {
     wg-quick down $Interface >/dev/null 2>&1
-    sleep 2
+    sleep 1
     wg-quick up $Interface >/dev/null 2>&1
     let Count++
     Test_Netflix_Access
@@ -41,7 +51,7 @@ function ChangeIP {
 
 function Judge {
     if [[ $Count == 0 ]];then
-        sleep 10
+        sleep 30
         echo -e " [Info] Still Working,Skipped..."
         Test_Netflix_Access
     else
