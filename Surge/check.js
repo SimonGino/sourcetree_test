4fb5ef34796b9fcedb6fe0503f7420e0b2d5af77
SimonGino <68073491+SimonGino@users.noreply.github.com>
2022-04-19 23:00:32 +0800

    Update check.js

--- Surge/check.js
+++ Surge/check.js
@@ -39,17 +39,17 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
  let disney_result=""
     if (status==STATUS_COMING) {
         //console.log(1)
-        disney_result="Disney+: 即将登陆："+region.toUpperCase()
+        disney_result="Disney+：即将登陆："+region.toUpperCase()
       } else if (status==STATUS_AVAILABLE){
         //console.log(2)
         console.log(region)
-        disney_result="Disney+: 已解锁，区域："+region.toUpperCase()
+        disney_result="Disney+：已解锁，区域："+region.toUpperCase()
         // console.log(result["Disney"])
       } else if (status==STATUS_NOT_AVAILABLE) {
         //console.log(3)
-        disney_result="Disney+:未支持 🚫 "
+        disney_result="Disney+：未支持 🚫 "
       } else if (status==STATUS_TIMEOUT) {
-        disney_result="Disney+:检测超时 🚦"
+        disney_result="Disney+：检测超时 🚦"
       }
 result.push(disney_result)
 console.log(result)
