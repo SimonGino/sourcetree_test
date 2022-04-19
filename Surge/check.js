4dcc2721bd963916025afba0d1795603fadd8c91
SimonGino <68073491+SimonGino@users.noreply.github.com>
2022-04-19 19:25:38 +0800

    Update check.js

--- Surge/check.js
+++ Surge/check.js
@@ -1,3 +1,7 @@
+let options = getOptions()
+let panel = {
+    title: options.title,
+  }
 const REQUEST_HEADERS = {
     'User-Agent':
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
