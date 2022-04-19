74ee969ea17e7d37a57ddc1aa4e6a831605fcfe6
SimonGino <68073491+SimonGino@users.noreply.github.com>
2022-04-19 19:18:34 +0800

    Update check.js

--- Surge/check.js
+++ Surge/check.js
@@ -93,49 +93,176 @@ const REQUEST_HEADERS = {
   }
 
   async function check_disney(){
-    let inner_check = ()=>{
-       return new Promise((resolve, reject) => {
-        let option={
-        url:'https://www.disneyplus.com/',
-        headers:REQUEST_HEADERS,        
-    }
-    $httpClient.get(opts, function (error, response, data) {
-        if (error) {
-          reject('Error')
-          return
+    try {
+        let { region, cnbl } = await Promise.race([testHomePage(), timeout(options.timeout)])
+        console.log(`homepage: region=${region}, cnbl=${cnbl}`)
+    
+        let { countryCode, inSupportedLocation, accessToken } = await Promise.race([getLocationInfo(), timeout(options.timeout)])
+        console.log(`getLocationInfo: countryCode=${countryCode}, inSupportedLocation=${inSupportedLocation}`)
+        let disney_check_result = 'Disney：'
+        region = countryCode ?? region
+        // 即将登陆
+        if (inSupportedLocation === false || inSupportedLocation === 'false') {
+            disney_check_result += '即将登陆' + region
+            return 
         }
-        if (response.status !== 200 || data.indexOf('unavailable') !== -1) {
-          reject('Not Available')
-          return
-        }
-  
-        let match = data.match(/Region: ([A-Za-z]{2})[\s\S]*?CNBL: ([12])/)
-        if (!match) {
-          resolve({ region: '', cnbl: '' })
-          return
-        }
-  
-        let region = match[1]
-        let cnbl = match[2]
-          resolve(region)
-      })
-    })
-    }
-
-    let disney_check_result = 'Disney：'
-
-    await inner_check()
-      .then((code) => {
-        if (code === 'Not Available') {
+    
+        let support = await Promise.race([testPublicGraphqlAPI(accessToken), timeout(options.timeout)])
+        if (!support) {
             disney_check_result += '不支持解锁'
-        } else {
-            disney_check_result += '已解锁，区域：' + code.toUpperCase()
+          return
         }
-      })
-      .catch((error) => {
+        // 支持解锁
+        disney_check_result += '已解锁，区域：' + region
+        return
+      } catch (error) {
+        console.log(error)
+    
+        // 不支持解锁
+        if (error === 'Not Available') {
+            disney_check_result += '不支持解锁'
+          return 
+        }
+    
+        // 检测超时
+        if (error === 'Timeout') {
+            disney_check_result += '检测超时'
+          return 
+        }
+    
         disney_check_result += '检测失败，请刷新面板'
-      })
-  
-    return disney_check_result
+        return 
+      }
+
+      function testPublicGraphqlAPI(accessToken) {
+        return new Promise((resolve, reject) => {
+          let opts = {
+            url: 'https://disney.api.edge.bamgrid.com/v1/public/graphql',
+            headers: {
+              'Accept-Language': 'en',
+              Authorization: accessToken,
+              'Content-Type': 'application/json',
+              'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36',
+            },
+            body: JSON.stringify({
+              query:
+                'query($preferredLanguages: [String!]!, $version: String) {globalization(version: $version) { uiLanguage(preferredLanguages: $preferredLanguages) }}',
+              variables: { version: '1.5.0', preferredLanguages: ['en'] },
+            }),
+          }
+      
+          $httpClient.post(opts, function (error, response, data) {
+            if (error) {
+              reject('Error')
+              return
+            }
+            resolve(response.status === 200)
+          })
+        })
+      }
+      
+      function getLocationInfo() {
+        return new Promise((resolve, reject) => {
+          let opts = {
+            url: 'https://disney.api.edge.bamgrid.com/graph/v1/device/graphql',
+            headers: {
+              'Accept-Language': 'en',
+              Authorization: 'ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84',
+              'Content-Type': 'application/json',
+              'User-Agent': UA,
+            },
+            body: JSON.stringify({
+              query: 'mutation registerDevice($input: RegisterDeviceInput!) { registerDevice(registerDevice: $input) { grant { grantType assertion } } }',
+              variables: {
+                input: {
+                  applicationRuntime: 'chrome',
+                  attributes: {
+                    browserName: 'chrome',
+                    browserVersion: '94.0.4606',
+                    manufacturer: 'apple',
+                    model: null,
+                    operatingSystem: 'macintosh',
+                    operatingSystemVersion: '10.15.7',
+                    osDeviceIds: [],
+                  },
+                  deviceFamily: 'browser',
+                  deviceLanguage: 'en',
+                  deviceProfile: 'macosx',
+                },
+              },
+            }),
+          }
+      
+          $httpClient.post(opts, function (error, response, data) {
+            if (error) {
+              reject('Error')
+              return
+            }
+      
+            if (response.status !== 200) {
+              console.log('getLocationInfo: ' + data)
+              reject('Not Available')
+              return
+            }
+      
+            data = JSON.parse(data)
+            if(data?.errors){
+              console.log('getLocationInfo: ' + data)
+              reject('Not Available')
+              return
+            }
+      
+            let {
+              token: { accessToken },
+              session: {
+                inSupportedLocation,
+                location: { countryCode },
+              },
+            } = data?.extensions?.sdk
+            resolve({ inSupportedLocation, countryCode, accessToken })
+          })
+        })
+      }
+      
+      function testHomePage() {
+        return new Promise((resolve, reject) => {
+          let opts = {
+            url: 'https://www.disneyplus.com/',
+            headers: {
+              'Accept-Language': 'en',
+              'User-Agent': UA,
+            },
+          }
+      
+          $httpClient.get(opts, function (error, response, data) {
+            if (error) {
+              reject('Error')
+              return
+            }
+            if (response.status !== 200 || data.indexOf('unavailable') !== -1) {
+              reject('Not Available')
+              return
+            }
+      
+            let match = data.match(/Region: ([A-Za-z]{2})[\s\S]*?CNBL: ([12])/)
+            if (!match) {
+              resolve({ region: '', cnbl: '' })
+              return
+            }
+      
+            let region = match[1]
+            let cnbl = match[2]
+            resolve({ region, cnbl })
+          })
+        })
+      }
+      
+      function timeout(delay = 5000) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            reject('Timeout')
+          }, delay)
+        })
+      }
     
   }
